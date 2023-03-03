3ad44fc481a9e06e122051b66b850e8c1c0c936e
Paola Sanchez <yineth2paola2@gmail.com>
2023-03-02 21:29:30 -0500

    Funtion fix in Admin

--- src/controllers/AdminListController.js
+++ src/controllers/AdminListController.js
@@ -1,26 +1,25 @@
 const PersonServices = require ('../services/PersonServices');
 const Person = require('../models/personsModel');
 const bcrypt = require ("bcrypt");
-//const { profile } = require('console');
 
-//class adminList {
-//    async getAdminList(req, res) {
-//      const adminList = await Person.getAdminList();
-//      res.status(adminList);
+//const getAdminList = (req, res) => {
+    //const {profilePerson} = req.body
+//    const getAdmin = await Person.find ({profilePerson : 2}).exec();
+//    if(filter == 2){
+//        Person.find((err, result) => {
+//            if (err) throw new Error(err);
+//            res.json(result);
+//            console.log(result);
+//          });
 //    }
-//  }
-
-const getAdminList = (req, res) => {
-    const {profilePerson} = req.body
-    const filter = Person.find ({profilePerson}).exec();
-    if(filter == 2){
-        Person.find((err, result) => {
-            if (err) throw new Error(err);
-            res.json(result);
-            console.log(result);
-          });
-    }
-}
+//}
   
-  //module.exports = new adminList();
+const getAdminList = async (req, res) => {
+  try{
+    const getAdmin = await Person.find({ profilePerson : 2 });
+    res.json(getAdmin);
+  } catch (err) {
+    res.json({ message: err.message});
+  }
+};
   module.exports ={getAdminList};
