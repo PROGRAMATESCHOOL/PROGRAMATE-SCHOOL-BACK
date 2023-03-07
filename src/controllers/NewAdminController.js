7d8a76db5d07c838474f76ab3de8525af72c9f65
Jajaharold <haroldurrea@gmail.com>
2023-03-06 21:34:46 -0500

    Branch Fix

--- src/controllers/NewAdminController.js
+++ src/controllers/NewAdminController.js
@@ -3,24 +3,23 @@ const Person = require("../models/personsModel");
 const uniqid = require("uniqid");
 
 const NewAdmin = async (req, res) => {
-  const {
-    name1Person,
-    name2Person,
-    lastname1Person,
-    lastname2Person,
-    documentPerson,
-    profilePerson,
-    agePerson,
-  } = req.body; //Parse the request for using data
+    const {
+        name1Person,
+        name2Person,
+        lastname1Person,
+        lastname2Person,
+        documentPerson,
+        emailPerson,
+        agePerson,
+    } = req.body; //Parse the request for using data
 
-  const existentAdmin = await Person.findOne({ emailPerson }).exec();
-  const existentDocumentAdmin = await Person.findOne({ documentPerson }).exec(); //searches for an existing user with the same document or email
+    const existentAdmin = await Person.findOne({ emailPerson }).exec();
+    const existentDocumentAdmin = await Person.findOne({ documentPerson }).exec(); //searches for an existing user with the same document or email
 
   if (existentAdmin || existentDocumentAdmin) {
     res.status(409).send({ status: "Admin already exists" }); //Compares in case already exists and return error code 
     return;
   } else {
-    const profilePerson = 2;
     if (profilePerson == 2) {
       const passwordPerson = uniqid(undefined, lastname1Person);
       
@@ -37,18 +36,21 @@ const NewAdmin = async (req, res) => {
         agePerson: agePerson,
       });
 
-      createNewAdmin.save();
-      res
-        .status(201)
-        .send({ status: "New admin created", data: createNewAdmin });
+            createNewAdmin.save();
+                res
+                    .status(201)
+                    .send({ status: "New admin created", data: createNewAdmin });
 
-    
-      
-      return;
-    } else { //if profile person is not 2 return error code, might change in future updates
-      res.status(401).send({ status: "Not an allowed profile" }); 
+        
+        
+            return;
+        } else { //if profile person is not 2 return error code, might change in future updates
+            res.status(401).send({ status: "Not an allowed profile" }); 
+        }
     }
-  }
 };
 
-module.exports = { NewAdmin };
+module.exports = {
+    NewAdmin
+};
+
