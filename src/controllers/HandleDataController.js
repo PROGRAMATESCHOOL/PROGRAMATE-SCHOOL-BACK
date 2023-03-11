99b030b8ce9676139a9817e5accd25398973b3f4
manuel101284 <94885870+manuel101284@users.noreply.github.com>
2023-03-11 02:40:32 -0500

    Update HandleDataController.js

--- src/controllers/HandleDataController.js
+++ src/controllers/HandleDataController.js
@@ -1,26 +1,50 @@
 const personServices = require("../services/PersonServices");
-const ScoreForm = require("../models/scoreAnnouncementModel");
+const Score = require("../models/scoreAnnouncementModel");
 const bcrypt = require("bcrypt");
 const Person = require("../models/personsModel");
+const Questionary = require("../models/questionaryModel");
+const Announcement = require("../models/announcementsModel");
 
 const scoreForm = async (req, res) => {
   const {
-    ScoreProfile,
-    ScoreVocation,
-    ScoreMotivation,
-    ScoreLogic,
-    ScoreTotal,
-  } = req.body;
+    documentPerson,
+    nameAnnouncement, // This fileld will be export to the object ScoreForm
+  } = req.body
 
-  const scoreform = new ScoreForm({
-    ScoreProfile: ScoreProfile,
-    ScoreVocation: ScoreVocation,
-    ScoreMotivation: ScoreMotivation,
-    ScoreLogic: ScoreLogic,
-    ScoreTotal: ScoreTotal,
-  });
+    const StundentReg = await Person.findOne({ documentPerson }).exec();
 
-  scoreform.save().then((result) => console.log("Score Saved"));
+    const AnnouncementReg = await Announcement.findOne({nameAnnouncement}).exec();
+
+    const StudentQuestionary = await Questionary.findone({documentPerson, nameAnnouncement}).exec();
+
+    const idAnnouncement = AnnouncementReg._id
+    // constt nameannouncement = already exist
+    const idStudent = Person._id
+    const name1Student = StundentReg.name1Person
+    const lastname1Student = StundentReg.lastname1Person
+    const ScoreProfile = StudentQuestionary.form_ScoreProfile
+    const ScoreVocation = StudentQuestionary.form_ScoreVocation
+    const ScoreMotivation = StudentQuestionary.form_ScoreMotivation
+    const ScoreLogic = StudentQuestionary.form_ScoreLogic
+    const ScoreTotal = StudentQuestionary.form_ScoreTotal
+    const stateAnnouncementStudent = StudentQuestionary.form_statusAnnouncement
+
+    const NewScoreStudent = new Score({
+      idAnnouncement: idAnnouncement,
+      nameAnnouncement: nameAnnouncement,
+      name1Student: name1Student,
+      lastname1Student: lastname1Student,
+      ScoreProfile: ScoreProfile,
+      ScoreVocation: ScoreVocation,
+      ScoreMotivation: ScoreMotivation,
+      ScoreLogic: ScoreLogic,
+      ScoreTotal: ScoreTotal,
+      stateAnnouncementStudent: stateAnnouncementStudent
+    });
+
+    NewScoreStudent.save()
+    console.log("Registro de puntajes exitoso")
+    res.send(NewScoreStudent)
 };
 
 module.exports = {
