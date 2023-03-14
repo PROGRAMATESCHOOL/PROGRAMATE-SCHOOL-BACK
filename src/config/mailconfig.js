718d9cc53ff901669aa34f1a53a83d1f66d22a5c
Jajaharold <haroldurrea@gmail.com>
2023-03-14 16:03:01 -0500

    fixx v2

--- src/config/mailconfig.js
+++ src/config/mailconfig.js
@@ -7,36 +7,34 @@ require("dotenv").config();
 //For this test, I used a google mail that is save in the .env, also the data in the constant 'transport'is the mailtrap.
 
 let transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.userEmail,
-      pass: process.env.passEmail
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.userEmail,
+    pass: process.env.passEmail,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
-const sendEmail = async (emailPerson, subject, html ) => {
-    try {
-        await transport.sendMail({
-            from: ` ProgramateSchool <$( process.env.userEmail )>` ,// sender address
-            to: emailPerson, // list of receivers
-            subject,// Subject line
-            // text: "Holaaaa, vamos a verificar tu correo", // plain text body
-            html,
-        });
-
-    } catch (error) {
-        console.log('Algo no va bien con el Email', error);
-        // res
-        // .status (401)
-        // .send({status: 'Error con el email', data:sendEmail})     
-          
-    }     
-}
+const sendEmail = async (emailPerson, subject, html) => {
+  try {
+    await transport.sendMail({
+      from: ` ProgramateSchool <$( process.env.userEmail )>`, // sender address
+      to: emailPerson, // list of receivers
+      subject, // Subject line
+      // text: "Holaaaa, vamos a verificar tu correo", // plain text body
+      html,
+    });
+  } catch (error) {
+    console.log("Algo no va bien con el Email", error);
+    // res
+    // .status (401)
+    // .send({status: 'Error con el email', data:sendEmail})
+  }
+};
 
 const getTemplate = (name1Person, lastname1Person, token) => {
   return `
@@ -56,15 +54,20 @@ const getTemplate = (name1Person, lastname1Person, token) => {
     `;
 };
 
-const getTemplatePassword = (name1Person, lastname1Person, emailPerson, passwordPerson) => {
-    return `
+const getTemplatePassword = (
+  name1Person,
+  lastname1Person,
+  emailPerson,
+  passwordPerson
+) => {
+  return `
       <head>
           <link rel="stylesheet" href="./style.css">
       </head>
       
       <div id="email___content">
           <img src="" alt="">
-          <h2>Hola ${ name1Person + " "+ lastname1Person }</h2>
+          <h2>Hola ${name1Person + " " + lastname1Person}</h2>
           <p>Tu cuenta de correo ha sido verificada con exito. A continuación en contraras tu usuario y contraseña para acceder a la pagina de Programte School donde podras conocer e inscribirte a nuestras convocatorias.
           </p>
 
@@ -73,11 +76,10 @@ const getTemplatePassword = (name1Person, lastname1Person, emailPerson, password
           
       </div>
     `;
-}
+};
 
-
-module.exports={
-    sendEmail,
-    getTemplate,
-    getTemplatePassword,
-}
+module.exports = {
+  sendEmail,
+  getTemplate,
+  getTemplatePassword,
+};
