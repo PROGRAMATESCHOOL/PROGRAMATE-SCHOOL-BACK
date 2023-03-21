1dc33fae2c52daa8e98a9f24e96bf44fde2f06cb
Jajaharold <haroldurrea@gmail.com>
2023-03-20 19:42:48 -0500

    Branch Merge fixxed with F/leidy

--- src/routes/v1/PersonsRoutes.js
+++ src/routes/v1/PersonsRoutes.js
@@ -4,11 +4,11 @@ const GetAllPersonsController = require("../../controllers/GetAllPersonsControll
 const SignUp = require("../../controllers/RegisterController");
 const NewAdmin = require("../../controllers/NewAdminController");
 const AdminListController = require("../../controllers/AdminListController");
-const StudentListController = require("../../controllers/StudentListController") 
-const NewAnnouncement = require("../../controllers/NewAnnouncementController")
+const StudentListController = require("../../controllers/StudentListController");
+const NewAnnouncement = require("../../controllers/NewAnnouncementController");
 const DeleteAnnouncement = require("../../controllers/DeleteAnnouncementController");
-const SignUpInAnnouncement = require("../../controllers/RegisterToAnnouncementController")
-const disableAnnouncement = require("../../controllers/DisabledAnnouncementController")
+const SignUpInAnnouncement = require("../../controllers/RegisterToAnnouncementController");
+const disableAnnouncement = require("../../controllers/DisabledAnnouncementController");
 
 const checkAuth = require("../../middleware/authentication");
 const checkProfileAuth = require("../../middleware/RoleAuth");
@@ -16,12 +16,7 @@ const checkProfileAuth = require("../../middleware/RoleAuth");
 //This is an inicial version of the router, might change due to Functionality & new features
 
 router
-  .get(
-    "/persons",
-    checkAuth,
-    checkProfileAuth(["Admin", "SuperAdmin"]),
-    GetAllPersonsController.getAllPersons
-  ) //Custom route used to get all persons
+  .get("/persons",checkAuth,checkProfileAuth(["Admin", "SuperAdmin"]),GetAllPersonsController.getAllPersons) //Custom route used to get all persons
   .get("/getAdmin", AdminListController.getAdminList) // Custom route used to get all Admins
   .get("/getUsers", StudentListController.getAllStudents) //Custom route used to get all students
   .get("/confirm/:token", SignUp.confirm)
@@ -30,12 +25,10 @@ router
   .post("/newAdmin", NewAdmin.NewAdmin)
   //.post("/scoreannouncement", ScoreAnnouncement.scoreForm)
   .post("/addnewannouncement", NewAnnouncement.AddAnnouncement) //Custom route used to create announcement
-  
-  .post("/registertoannouncement", SignUpInAnnouncement.RegisterToAnnouncement) //Custom route used to register announcement 
-  
-  .delete("/DeleteAnnouncement", DeleteAnnouncement.deleteAnnouncement) //Custom route used to delete announcement
+
+  .post("/registertoannouncement", SignUpInAnnouncement.RegisterToAnnouncement) //Custom route used to register announcement
   .patch("/disabledAnnouncement/:id", disableAnnouncement.disableAnnouncement) //Custom route used to disable announcement
 
-  
-  
+  .delete("/DeleteAnnouncement", DeleteAnnouncement.deleteAnnouncement); //Custom route used to delete announcement
+
 module.exports = router;
