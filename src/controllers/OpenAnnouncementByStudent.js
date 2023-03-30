a6a372c21b485c302f087ff4d4eecdcbd7c86c65
manuel101284 <94885870+manuel101284@users.noreply.github.com>
2023-03-30 18:58:32 -0500

    Update OpenAnnouncementByStudent.js

--- src/controllers/OpenAnnouncementByStudent.js
+++ src/controllers/OpenAnnouncementByStudent.js
@@ -1,27 +1,38 @@
-const Person = require("../models/personsModel");
-const Questionary = require("../models/questionaryModel");
-const Announcement = require("../models/announcementsModel");
+const Person = require("../models/personsModel")
+const Questionary = require("../models/questionaryModel")
+const Announcement = require("../models/announcementsModel")
 
 const OpenAnnouncement = async (req, res) => {
-  // THIS  VARIABLE MUST COME FROM TOKEN OR COOKIES
-  const { idPerson } = req.body;
+    
+    // THIS  VARIABLE MUST COME FROM TOKEN OR COOKIES
+    const { idPerson } = req.body
 
-  //const idStu = await Person.findOne({documentPerson: documentPerson}, {_id:1}).exec()
+    const idStudent = await Person.findOne({_id: idPerson}, {_id:1}).exec()
+    console.log(idStudent)
 
-  const AllAnnouncements = await Announcement.find({
-    $or: [
-      { studentsRegistered: { $nin: [idPerson] } },
-      { studentsRegistered: { $exists: false } },
-    ],
-  });
+    if (idStudent) {
 
-  if (AllAnnouncements) {
-    res.status(200).send(AllAnnouncements);
-  } else {
-    res.status(201).send("No hay convocatorias disponibles Ahora");
-  }
-};
+        const AllAnnouncements = await Announcement.find( {$or: [ {studentsRegistered: { $nin: [idStudent] }}, {studentsRegistered: { $exists: false}}]})
+        console.log(AllAnnouncements)
 
+        if(!AllAnnouncements){
+            res.status(200)
+            res.send("IN PROCESS")
+        }
+        if(AllAnnouncements){
+            res.status(200)
+            res.send("OPEN")
+        } 
+        else {
+            res.status(201)
+            res.send("No hay convocatorias disponibles Ahora")
+        }
+    }
+    if(!idStudent) {
+        res.send("ESTUDIANTE NO ENCONTRADO")
+    }
+}
+    
 module.exports = {
-  OpenAnnouncement,
-};
+    OpenAnnouncement
+}
