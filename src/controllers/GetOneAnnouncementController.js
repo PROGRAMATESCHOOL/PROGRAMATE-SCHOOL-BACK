42754bf718a3e06150a764cd6673b18c72fc9a36
manuel101284 <94885870+manuel101284@users.noreply.github.com>
2023-03-30 18:53:48 -0500

    Update GetOneAnnouncementController.js

--- src/controllers/GetOneAnnouncementController.js
+++ src/controllers/GetOneAnnouncementController.js
@@ -1,32 +1,28 @@
-const Questionary = require("../models/questionaryModel");
-const Announcement = require("../models/announcementsModel");
+const Questionary = require("../models/questionaryModel")
+const Announcement = require("../models/announcementsModel")
 
-const getOneAnnouncement = async (req, res) => {
-  const { idPerson } = req.body;
+const getOneAnnouncement = async (req,res) => {
+    const {idPerson} = req.body
 
-  try {
-    const oneAnnouncement = await Questionary.findOne({ idStudent: idPerson });
-
-    var oneAnnoun;
-
-    if (oneAnnouncement) {
-      const idAnnoun = oneAnnouncement.idAnnouncement;
-
-      const Announ = await Announcement.findOne({ _id: idAnnoun });
-      oneAnnoun = Announ.nameAnnouncement;
-      res.status(200).json(oneAnnoun);
-    } else {
-      nameAnnoun =
-        "No está aplicando a niguna convocatoria. Aplica ahora mismo";
-      res
-        .status(200)
-        .json("No está aplicando a niguna convocatoria. Aplica ahora mismo");
+    try {
+        const oneAnnouncement = await Questionary.findOne({idStudent: idPerson})
+        
+        if(oneAnnouncement) {
+            const idAnnoun = oneAnnouncement.idAnnouncement
+            
+            const Announ = await Announcement.findOne({_id: idAnnoun})
+            const  oneAnnoun = Announ.nameAnnouncement
+            res.status(200).json(oneAnnoun)
+        }
+        else {
+            const nameAnnoun = "No estás aplicando a ninguna convocatoria. Aplica ahora mismo"
+            res.status(200).json(nameAnnoun)
+        }
+    } catch(err) {
+        res.status(500).json({ message: err.message })
     }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+}
 
 module.exports = {
-  getOneAnnouncement,
-};
+    getOneAnnouncement
+}
